25bb003aaa353870c8c42f817d4a5722803839ae
kford <kford@avenueconsultants.com>
2025-06-17 12:30:07 -0600

    Adding in the modified key for the event log utility

---
+++ Atspm/PostgreSQLDatabaseProvider/Migrations/EventLog/20250617172634_V5_ModifyKeyForCompressedEvents.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Utah.Udot.ATSPM.PostgreSQLDatabaseProvider.Migrations.EventLog
+{
+    /// <inheritdoc />
+    public partial class V5_ModifyKeyForCompressedEvents : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CompressedEvents",
+                table: "CompressedEvents");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CompressedEventsWithType",
+                table: "CompressedEvents",
+                columns: new[] { "LocationIdentifier", "DeviceId", "DataType", "Start", "End" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CompressedEvents",
+                table: "CompressedEvents");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CompressedEventsWithType",
+                table: "CompressedEvents",
+                columns: new[] { "LocationIdentifier", "DeviceId", "Start", "End" });
+        }
+    }
+}
